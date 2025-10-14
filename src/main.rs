6a7032014f279d245e54e33e7308ec40b52abdf1
Chris <chrisgaines47@gmail.com>
2025-10-14 16:46:24 -0700

    Configure generic 3 step operation. Read File -> Solve Puzzle -> Output to File

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+//stores an individual SudokuBox
+struct SudokuBox {}
+
+//stores a 3x3 grid of SudokuBoxes
+struct SudokuSquare {}
+
+//stores a 3x3 grid of SudokuSquares
+struct SudokuPuzzle {}
+
+//reads an input file and returns a created SudokuPuzzle
+fn get_sudoku_puzzle() -> SudokuPuzzle {
+    return SudokuPuzzle {};
+}
+
+//takes an unsolved sudoku puzzle as input and returns a solved sudoku puzzle as output
+fn solve_sudoku_puzzle(_unsolved_sudoku_puzzle: SudokuPuzzle) -> SudokuPuzzle {
+    let solved_sudoku_puzzle = _unsolved_sudoku_puzzle;
+    return solved_sudoku_puzzle;
+}
+
+//takes a solved SudokuPuzzle and writes it to an output file
+fn output_solved_puzzle(_solved_sudoku_puzzle: SudokuPuzzle) {
+    //output to file here
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Sudoku Solver: start");
+
+    let sudoku_puzzle = get_sudoku_puzzle();
+    let solved_sudoku_puzzle = solve_sudoku_puzzle(sudoku_puzzle);
+    output_solved_puzzle(solved_sudoku_puzzle);
 }
